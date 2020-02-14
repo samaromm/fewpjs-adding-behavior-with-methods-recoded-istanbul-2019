3b3d7aabcde750e9f4c211d9d9b8add576986031
Samar Omar <samaromm98@gmail.com>
2020-02-14 13:03:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ class Bird{
   }
   speak(){
     if(this.sex=='male') return 'It\'s me! '+this.name+', the parrot!'
-      else return this.name+' says woof!'
+      else return this.name+' says squawk!'
   }
 }
